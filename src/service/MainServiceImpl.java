afcc9d1d14dc67e61dd09db308c0e1f8affa6c69
Oleksandr <70403509@gmx.de>
2025-03-21 10:59:57 +0100

    Дописал методы User

--- src/service/MainServiceImpl.java
+++ src/service/MainServiceImpl.java
@@ -2,72 +2,122 @@ package service;
 
 import model.Book;
 import model.User;
+import repository.BookRepository;
+import repository.UserRepository;
 import utils.MyList;
+import utils.UserValidation;
 
 public class MainServiceImpl implements MainService {
+
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
+    private User activeUser;
+
+    public MainServiceImpl(BookRepository bookRepository, UserRepository userRepository) {
+        this.bookRepository = bookRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User registerUser(String email, String password) {
-        return null;
+        if (!UserValidation.isEmailValid(email)) {
+            System.out.println("Email не прошел проверку!");
+            return null;
+        }
+
+        if (!UserValidation.isPasswordValid(password)) {
+            System.out.println("Пароль не прошел проверку");
+            return null;
+        }
+
+        if (userRepository.getUserByEmail(email) != null) {
+            System.out.println("Пользователь с таким email уже существует");
+            return null;
+        }
+
+        User user = userRepository.addUser(email, password);
+        return user;
     }
 
     @Override
     public boolean loginUser(String email, String password) {
+        User user = userRepository.getUserByEmail(email);
+        if (user == null) return false;
+
+        if (user.getPassword().equals(password)) {
+            this.activeUser = user;
+            return true;
+        }
         return false;
     }
 
     @Override
     public void logout() {
+        this.activeUser = null;
+    }
 
+    @Override
+    public User getActiveUser() {
+        return this.activeUser;
     }
 
     @Override
     public boolean borrowBook(int bookId) {
-        return false;
+        Book book =  bookRepository.getBookById(bookId);
+        if (book == null || book.isBusy()) return false;
+        book.setBusy(true);
+        return true;
     }
 
     @Override
     public boolean returnBook(int bookId) {
-        return false;
-    }
-
-    @Override
-    public void editBook(String title, String author, int year, int pages) {
-
+        Book book =  bookRepository.getBookById(bookId);
+        if (book == null || !book.isBusy()) return false;
+        book.setBusy(false);
+        return true;
     }
 
     @Override
     public MyList<Book> getBooksByTitle(String title) {
-        return null;
+        return bookRepository.getBooksByTitle(title);
     }
 
     @Override
     public MyList<Book> getBooksByAuthor(String author) {
-        return null;
+        return bookRepository.getBooksByAuthor(author);
     }
 
     @Override
     public MyList<Book> getAllBooks() {
-        return null;
+        return bookRepository.getAllBooks();
     }
 
     @Override
     public MyList<Book> getAvailableBooks() {
-        return null;
+        return bookRepository.getAvailableBooks();
     }
 
     @Override
     public MyList<Book> getBorrowedBooks() {
-        return null;
-    }
-
-
-    @Override
-    public User getActiveUser() {
-        return null;
+        return bookRepository.getBorrowedBooks();
     }
 
     @Override
     public Book addBook(String title, String author, int year, int pages) {
-        return null;
+        return bookRepository.addBook(title, author, year, pages);
     }
+
+
+    @Override
+    public boolean editBook(Book book, String title, String author, int year, int pages) {
+        if (book == null) return false;
+
+        if (title != null) book.setTitle(title);
+        if (author != null) book.setAuthor(author);
+        if (year != 0) book.setYear(year);
+        if (pages != 0) book.setPages(pages);
+        bookRepository.saveBook(book);
+        return true;
+    }
+
 }
