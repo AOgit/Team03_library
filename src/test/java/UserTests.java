cf6e70b6d767257d4634023d2e4d797140200ae9
Kostiantyn Teimurov <tejmurovkostia111@gmail.com>
2025-03-24 22:56:47 +0100

    test

--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -3,6 +3,8 @@ import model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import repository.BookRepository;
 import repository.BookRepositoryImpl;
 import repository.UserRepository;
@@ -16,12 +18,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserTests {
     private User superAdmin;
     private User user;
-    private Book book1;
     private UserRepository userRepository;
     private BookRepository bookRepository;
     private MainService service;
     private final String emailSuper = "superAdmin@mail.de";
-    private final String passwordSeuper = "superAdmin";
+    private final String passwordSuper = "superAdmin";
 
     private final String email = "user@mail.de";
     private final String password = "user";
@@ -31,15 +32,13 @@ public class UserTests {
         userRepository = new UserRepositoryImpl();
         bookRepository = new BookRepositoryImpl();
         service = new MainServiceImpl(bookRepository, userRepository);
-//        superAdmin = new User(emailSuper, passwordSeuper);
-//        user = new User(email, password);
-        service.loginUser(emailSuper, passwordSeuper);
-
-        book1 = new Book(1, "Java для чайников", "Берд Б.", 2013, 368, "сказка");
+        service.loginUser(emailSuper, passwordSuper);
     }
 
     @Test
     void createUser() {
+        assertNull(superAdmin);
+        superAdmin = new User(emailSuper, passwordSuper);
         assertNotNull(superAdmin);
     }
 
@@ -63,25 +62,40 @@ public class UserTests {
     }
 
     @Test
-    void getAllUsers() {
+    void testGetAllUsers() {
         MyList<User> allUsers = service.getAllUsers();
         assertNotNull(allUsers);
         int size = allUsers.size();
         service.registerUser("email@gmail.com", "Password111_");
         assertEquals(size + 1, allUsers.size());
 
+        user = new User(email, password);
+
         service.deleteUser(user);
         assertEquals(size, allUsers.size());
     }
 
     @Test
-    void getAllReaders() {
+    void testGetAllReaders() {
         assertTrue(service.getAllReaders().isEmpty());
         assertEquals(0, service.getAllReaders().size());
 
         service.borrowBook(1);
         assertEquals(1, service.getAllReaders().size());
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "email.com", "незарегистрированный email"})
+    void testGetUserByInvalidEmail(String emails) {
+        assertNull(service.getUserByEmail(emails), "Если такой email есть, то пользователь != null.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {email, emailSuper, "admin@mail.de"})
+    void testGetUserByValidEmail(String emails) {
+        assertNotNull(service.getUserByEmail(emails));
+    }
+
 
 
 
