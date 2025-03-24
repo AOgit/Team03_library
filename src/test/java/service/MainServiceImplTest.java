8082ecafccc695c5acfbdf7e9a7c7446a1b51056
Eva Dieterle <eva.dieterle.esl@gmail.com>
2025-03-24 16:45:40 +0100

    Добавила тесты

--- src/test/java/service/MainServiceImplTest.java
+++ src/test/java/service/MainServiceImplTest.java
@@ -19,6 +19,12 @@ class MainServiceImplTest {
     private User superAdmin;
     private User admin;
     private User user;
+//    private final String emailSuper = "superAdmin@mail.de";
+//    private final String passwordSuper = "superAdmin";
+//    private final String emailAdmin = "admin@mail.de";
+//    private final String passwordAdmin = "admin";
+//    private final String email = "user@mail.de";
+//    private final String password = "user";
 
 
  @BeforeEach
@@ -27,28 +33,28 @@ class MainServiceImplTest {
   bookRepository = new BookRepositoryImpl();
   mainService = new MainServiceImpl(bookRepository, userRepository);
 
-  superAdmin = new User("superAdmin@mail.de", "SuperAdmin1!");
-  superAdmin.setRole(Role.SUPER_ADMIN);
-
-  admin = new User("admin@mail.de", "Admin1234!");
-  admin.setRole(Role.ADMIN);
-
-  user = new User("user@mail.de", "User1234!");
-  user.setRole(Role.USER);
+//  superAdmin = new User("superAdmin@mail.de", "SuperAdmin1!");
+//  superAdmin.setRole(Role.SUPER_ADMIN);
+//
+//  admin = new User("admin@mail.de", "Admin1234!");
+//  admin.setRole(Role.ADMIN);
+//
+//  user = new User("user@mail.de", "User1234!");
+//  user.setRole(Role.USER);
 
  }
 
- @Test
- void testAddUser() {
-
-  assertNotNull(superAdmin, "SuperAdmin не был добавлен!");
-  assertNotNull(admin, "Admin не был добавлен!");
-  assertNotNull(user, "User не был добавлен!");
-
-  assertEquals(Role.SUPER_ADMIN, superAdmin.getRole(), "Роль для SuperAdmin неверна!");
-  assertEquals(Role.ADMIN, admin.getRole(), "Роль для Admin неверна!");
-  assertEquals(Role.USER, user.getRole(), "Роль для User неверна!");
- }
+// @Test
+// void testAddUser() {
+//
+//  assertNotNull(superAdmin, "SuperAdmin не был добавлен!");
+//  assertNotNull(admin, "Admin не был добавлен!");
+//  assertNotNull(user, "User не был добавлен!");
+//
+//  assertEquals(Role.SUPER_ADMIN, superAdmin.getRole(), "Роль для SuperAdmin неверна!");
+//  assertEquals(Role.ADMIN, admin.getRole(), "Роль для Admin неверна!");
+//  assertEquals(Role.USER, user.getRole(), "Роль для User неверна!");
+// }
 
  @Test
  void testAddUserDuplicateEmail() {
@@ -61,7 +67,7 @@ class MainServiceImplTest {
  // Падает
  @Test
   void testLoginSuccess() {
-  boolean result = mainService.loginUser("user@mail.de", "User1234!");
+  boolean result = mainService.loginUser("user@mail.de", "user");
   System.out.println("Результат логина: " + result);
   assertTrue(result, "Пользователь должен успешно войти!");
  }
@@ -78,6 +84,50 @@ class MainServiceImplTest {
   assertNull(mainService.getActiveUser(), "После выхода активный пользователь должен быть null");
 }
 
+     @Test
+    void testIsLoggedIn() {
+        assertFalse(mainService.isLoggedIn(), "До входа в систему пользователь не должен быть залогинен");
+
+        mainService.loginUser("user@mail.de", "user");
+        assertTrue(mainService.isLoggedIn(), "После входа в систему пользователь должен быть залогинен");
+
+        mainService.logout();
+        assertFalse(mainService.isLoggedIn(), "После выхода пользователь должен быть разлогинен");
+    }
+
+    @Test
+    void testIsAdmin() {
+        userRepository.addUser("admin1@mail.de", "Admin1234!!");
+        User adminUser = userRepository.getUserByEmail("admin1@mail.de");
+        adminUser.setRole(Role.ADMIN);
+
+//        mainService.loginUser("admin@mail.de", "admin");
+        mainService.loginUser("admin1@mail.de", "Admin1234!!");
+
+        assertTrue(mainService.isAdmin(), "Админ должен быть распознан как админ");
+        assertTrue(mainService.isLoggedIn(), "Пользователь вошел");
+        assertFalse(mainService.isSuperAdmin(), "Админ не должен быть супер-админом");
+    }
+
+    @Test
+    void isSuperAdmin() {
+
+        userRepository.addUser("Superadmin1@mail.de", "SuperAdmin1234!!");
+        User adminUser = userRepository.getUserByEmail("Superadmin1@mail.de");
+        adminUser.setRole(Role.SUPER_ADMIN);
+
+//        mainService.loginUser("admin@mail.de", "admin");
+        mainService.loginUser("Superadmin1@mail.de", "SuperAdmin1234!!");
+
+        assertTrue(mainService.isSuperAdmin(), "Супер-админ должен быть распознан как супер-админ");
+        assertTrue(mainService.isLoggedIn(), "Система должна считать, что пользователь вошел");
+
+    }
+
+    @Test
+    void getUserByEmail() {
+
+    }
     @Test
     void getActiveUser() {
     }
@@ -90,9 +140,6 @@ class MainServiceImplTest {
     void getAllReaders() {
     }
 
-    @Test
-    void getUserByEmail() {
-    }
 
     @Test
     void blockUser() {
@@ -106,17 +153,7 @@ class MainServiceImplTest {
     void getUserBooks() {
     }
 
-    @Test
-    void isLoggedIn() {
-    }
 
-    @Test
-    void isAdmin() {
-    }
-
-    @Test
-    void isSuperAdmin() {
-    }
 
     @Test
     void changeUserRole() {
